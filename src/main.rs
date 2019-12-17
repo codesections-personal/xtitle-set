610384aca64e2716a8ffa7433daac38c5fde2600
Daniel Sockwell <daniel@codesections.com>
2019-12-16 22:45:56 -0500

    Add clap help text

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use clap::{crate_authors, crate_name, crate_version, App};
 use run_script::run_script;
 fn main() {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .get_matches();
+
     let args = std::env::args().skip(1).collect::<String>();
     let (_, out, _) = run_script!(format!(
         r#"[ $(uname -s) = Linux ] && exec echo -e "\033]1;{args}\007\033]2;{args}\007\c""#,
